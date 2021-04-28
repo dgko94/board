5d584bb0242ad0ba912ce3497050fe0a17d0c25c
dgko94 <llkk1346@naver.com>
2021-04-28 17:54:03 +0900

    최초커밋

---
+++ src/main/java/co/kr/boardkodg/BoardkodgApplication.java
@@ -0,0 +1,13 @@
+package co.kr.boardkodg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardkodgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardkodgApplication.class, args);
+	}
+
+}
